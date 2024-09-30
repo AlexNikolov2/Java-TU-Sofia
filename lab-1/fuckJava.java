2f4086865c27d7fdaf9156e7104d3ab0d9faab4d
Aleks Nikolov <tcr.official.world@gmail.com>
2024-09-30 18:27:22 +0300

    i dunno, man. I dunno.

---
+++ lab-1/fuckJava.java
@@ -0,0 +1,13 @@
+public abstract class fuckJava {
+    public static void main(String[] args){
+        System.out.println("Result of a and b is:");
+    }
+    public static int calc(Integer[] args){
+        int a = 5;
+        int b = 6;
+
+        System.out.println(a + b);
+
+        return 0;
+    }
+}
