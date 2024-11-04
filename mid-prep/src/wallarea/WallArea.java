2a01842d4583159a3c06939db5fd062acd592b37
Aleks Nikolov <tcr.official.world@gmail.com>
2024-11-04 13:38:54 +0200

    it works, so...

--- mid-prep/src/wallarea/WallArea.java
+++ mid-prep/src/wallarea/WallArea.java
@@ -1,5 +1,49 @@
 package wallarea;
 
 public class WallArea {
+    private double width;
+    private double height;
     
+    public WallArea(){
+    }
+    public WallArea(double width, double height){
+        this.width = width;
+        this.height = height;
+        if(width < 0){
+            this.width = 0;
+        }
+        if(height < 0){
+            this.height = 0;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setHeight(double height) {
+        if(height > 0){
+            this.height = height;
+        }
+        else{
+            this.height = 0;
+        }
+    }
+
+    public void setWidth(double width) {
+        if(width > 0){
+            this.width = width;
+        }
+        else{
+            this.width = 0;
+        }
+    }
+
+    public double getArea(){
+        return width * height;
+    }
 }
