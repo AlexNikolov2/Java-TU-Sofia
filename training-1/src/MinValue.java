511adb5c4038e68126eae02d67334338b326e48f
Aleks Nikolov <tcr.official.world@gmail.com>
2024-11-26 20:30:56 +0200

    wtf bro

---
+++ training-1/src/MinValue.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+public class MinValue {
+    public static void main(String[] args){
+        Integer[] arr = {3,7,1,9,-2, 40, 5};
+
+        System.out.println(Collections.min(Arrays.asList(arr)));
+    }
+}
